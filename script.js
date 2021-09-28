f83897f31c7de382d3ae02a634ef2e6f4b96dc9a
jagwardo <45190235+jagwardo@users.noreply.github.com>
2021-09-28 12:15:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var year = new Date().getFullYear()+parseInt(years);
 function compute()
 {
     p = document.getElementById("principal").value;
+    if(p <= 0){
+        alert("Enter a positive number")
+    }
+    else{
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
 
 }
 
@@ -15,4 +21,4 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 }
 
-rate.addEventListener('onchange', updateRate())
+rate.addEventListener('onchange', updateRate);
